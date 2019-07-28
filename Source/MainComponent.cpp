902a2748b731266b2ea6348f1c165c78d79c1c59
KISAKA Toshihiro <ktoshihiro@gmail.com>
2019-07-28 19:55:33 +0900

    Button Wrapper Class を追加

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -8,30 +8,58 @@
 
 #include "MainComponent.h"
 
-//==============================================================================
-MainComponent::MainComponent()
-{
-    setSize (600, 400);
+
+template<class ButtonType>
+auto makeButton(ButtonType *buttonToMakeUnique) -> std::unique_ptr<ButtonType> {
+    return std::unique_ptr<ButtonType>(buttonToMakeUnique);
 }
 
-MainComponent::~MainComponent()
-{
+
+//==============================================================================
+MainComponent::MainComponent() {
+//    heapButton.reset(
+//            new HeapButtonWrapper<TextButton>([]() {
+//                DBG("you clicked the heap");
+//            }, makeButton(new TextButton("heap button"))));
+    heapButton.reset(
+            new HeapButtonWrapper<TextButton>([]() {
+                DBG("you clicked the heap");
+            }, new TextButton("heap button")));
+
+    tb.onClick = []() {
+        std::cout << "TextButton tb" << std::endl;
+    };
+
+    addAndMakeVisible(static_cast<Component *>(button));
+    addAndMakeVisible((Component *) *heapButton);
+    addAndMakeVisible(tb);
+
+    setSize(600, 400);
+}
+
+MainComponent::~MainComponent() {
 }
 
 //==============================================================================
-void MainComponent::paint (Graphics& g)
-{
+void MainComponent::paint(Graphics &g) {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::red);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+    g.setFont(Font(16.0f));
+    g.setColour(Colours::red);
+    g.drawText("Hello World!", getLocalBounds(), Justification::centred, true);
 }
 
-void MainComponent::resized()
-{
+void MainComponent::resized() {
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+
+//    button->setBounds(JUCE_LIVE_CONSTANT(10),
+//                      JUCE_LIVE_CONSTANT(10), 100, 30);
+//    (*heapButton)->setBounds(JUCE_LIVE_CONSTANT(210),
+//                             JUCE_LIVE_CONSTANT(10), 100, 30);
+    button->setBounds(10, 10, 100, 30);
+    (*heapButton)->setBounds(210, 10, 100, 30);
+    tb.setBounds(10, 60, 100, 30);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -10,26 +10,123 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+
+template<typename ButtonType>
+class ButtonWrapper : public Button::Listener {
+public:
+    template<typename ... Args>
+    // optional parameter typename
+    ButtonWrapper(std::function<void()> onClickHandler, Args &&... args)
+            :onClick(std::move(onClickHandler)),
+             button(std::forward<Args>(args) ...) {
+        button.addListener(this);
+    }
+
+    ~ButtonWrapper() {
+        button.removeListener(this);
+    }
+
+    void buttonClicked(Button *b) override {
+        if (onClick)
+            onClick();
+    }
+
+    explicit operator Component *() {
+        return &button;
+    }
+
+    explicit operator Component &() {
+        return button;
+    }
+
+    Button *operator->() {
+        return &button;
+    }
+
+private:
+    std::function<void()> onClick;
+    ButtonType button;
+};
+
+
+template<class ButtonType>
+class HeapButtonWrapper : public Button::Listener {
+public:
+//    template<typename ... Args>
+//    // optional parameter typename
+//    HeapButtonWrapper(std::function<void()> onClickHandler, Args &&... args)
+//            :onClick(std::move(onClickHandler)),
+//             button(std::forward<Args>(args) ...) {
+//        button.addListener(this);
+//    }
+    HeapButtonWrapper(std::function<void()> onClickHandler, std::unique_ptr<ButtonType> heapButton)
+            : onClick(std::move(onClickHandler)), button(std::move(heapButton)) {
+        setupButton();
+    }
+
+//    HeapButtonWrapper(std::function<void()> onClickHandler, ButtonType *rawButtonPtr)
+//            : onClick(std::move(onClickHandler)), button(rawButtonPtr) {
+//        setupButton();
+//    }
+    HeapButtonWrapper(std::function<void()> onClickHandler, ButtonType *rawButtonPtr)
+            : HeapButtonWrapper(onClickHandler, std::unique_ptr<ButtonType>(rawButtonPtr)) {}
+
+    void setupButton() {
+        jassert(button.get() != nullptr);
+        if (button.get())
+            button->addListener(this);
+    }
+
+    ~HeapButtonWrapper() {
+        if (button.get())
+            button->removeListener(this);
+    }
+
+    void buttonClicked(Button *b) override {
+        if (onClick)
+            onClick();
+    }
+
+    // because out unique_ptr<> might not be holding anything
+    explicit operator Component *() const {
+        return button.get();
+    }
+
+    ButtonType *operator->() {
+        return button.get();
+    }
+
+private:
+    std::function<void()> onClick;
+    std::unique_ptr<ButtonType> button; // ここが変わった
+};
+
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public Component
-{
+class MainComponent : public Component {
 public:
     //==============================================================================
     MainComponent();
+
     ~MainComponent();
 
     //==============================================================================
-    void paint (Graphics&) override;
+    void paint(Graphics &) override;
+
     void resized() override;
 
 private:
     //==============================================================================
     // Your private member variables go here...
-
+    ButtonWrapper<TextButton> button{[]() {
+        DBG("Text Button Clicked");
+    }, "text button"};
+    std::unique_ptr<HeapButtonWrapper<TextButton>> heapButton;
+    TextButton tb{"TextButton"};
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
